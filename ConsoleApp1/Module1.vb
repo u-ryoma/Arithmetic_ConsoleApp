7522205523e8df48d350d78f77eccdaf4bd22dc3
Joshua <ryomaechizen1515@gmail.com>
2025-09-05 17:57:23 +0800

    Update Module1.vb

--- ConsoleApp1/Module1.vb
+++ ConsoleApp1/Module1.vb
@@ -10,21 +10,30 @@ Module Module1
 
         Dim num1 As Integer
         Dim num2 As Integer
+        Dim input as String
         Dim userAct As String
         Dim result As Double
         Dim optr As String
 
 
         Console.WriteLine("Enter First Number: ")
-        num1 = Console.ReadLine()
+        input = Console.ReadLine()
+        if not Integer.TryParse(input,num1) Then
+            Console.WriteLine("Invalid input, Please enter a 'NUMBER' ")
+            Environment.exit(0)
+            end if
         Console.WriteLine("Enter Second Number: ")
-        num2 = Console.ReadLine()
+        input = Console.ReadLine()
+        if not Integer.Tryparse(input,num2) then
+                Console.WriteLine("Invalid input, Please enter a 'NUMBER' ")
+             Environment.exit(0)
+            end if
     
         Console.WriteLine("Do you want to calculate the number you enter?")
         userAct = Console.ReadLine()
 
 
-        If (userAct = "Yes") Then
+        If (userAct.ToLower() = "yes") Then
             Console.WriteLine("What operator you want to use : 1 - Addition, 2 - Subtraction, 3 - Multiply, 4 - Division")
             optr = Console.ReadLine()
         Else
@@ -46,9 +55,16 @@ Module Module1
                 Console.WriteLine("The product of number you pick is " + CStr(result))
                 Environment.Exit(0)
             Case "4"
+                if num1 = 0 or num2 = 0  Then
+                        Console.Writeline("You can't divide a number by 0")
+                Else
                 result = num1 / num2 
                 Console.WriteLine("The quotient of number you pick is " + CStr(result))
                 Environment.Exit(0)
+            End if
+
+            case else 
+                    Console.Writeline("Invalid, Please choose from 1,2,3 or 4 ")
 
         End Select
 
